0287faab3bbc01582c92fe478e32ba20160f1ec2
francoiducat <francois.ducatillon@decathlon.com>
2021-07-18 11:52:51 +0200

    customize django admin forms

--- django-project/polls/admin.py
+++ django-project/polls/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+#class ChoiceInline(admin.StackedInline):
+#    model = Choice
+#    extra = 2
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('The Question',               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
